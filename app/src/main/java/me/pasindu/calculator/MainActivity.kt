9c65d71ab00dec3991064ad3b3240c3e4c5df25a
pAkalpa <pasinduakalpa1998@gmail.com>
2022-02-12 14:02:57 +0530

    :tada: Initial Commit

---
+++ app/src/main/java/me/pasindu/calculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package me.pasindu.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Works",Toast.LENGTH_SHORT).show()
+    }
+}
