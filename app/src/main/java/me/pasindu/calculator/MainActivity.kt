a1073684ec61f78bd534c04b1553dd9985389e78
pAkalpa <pasinduakalpa1998@gmail.com>
2022-02-14 21:58:53 +0530

    :art: All buttons fixed except "=" button

--- app/src/main/java/me/pasindu/calculator/MainActivity.kt
+++ app/src/main/java/me/pasindu/calculator/MainActivity.kt
@@ -3,14 +3,52 @@ package me.pasindu.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import me.pasindu.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val contentView = binding.root
+        setContentView(contentView)
     }
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button Works",Toast.LENGTH_SHORT).show()
+        binding.tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View) {
+        binding.tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            binding.tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+     fun onOperator(view: View) {
+         if (lastNumeric && !isOperatorAdded(binding.tvInput.text.toString())) {
+             binding.tvInput.append((view as Button).text)
+             lastNumeric = false
+             lastDot = false
+         }
+     }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
     }
 }
